ff736d1eb5a3650b22c9162209969e459e85e121
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-08-24 18:04:25 +0530

    Small update

--- src/components/sections/Stack.tsx
+++ src/components/sections/Stack.tsx
@@ -1,7 +1,33 @@
 const Stack = () => {
   return (
-    <section>Stack</section>
-  )
-}
+    <section className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
+        <div>
+          <div className="relative flex justify-center items-center w-12 h-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-slate-900">
+            <svg
+              className="w-7 h-7 text-blue-600 dark:text-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+              <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+            </svg>
+          </div>
+          <div className="mt-5">
+            <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
+              Next.js
+            </h3>
+            <p className="mt-1 text-gray-600 dark:text-gray-400">
+              A production ready full-stack React Framework for Web
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Stack
+export default Stack;
